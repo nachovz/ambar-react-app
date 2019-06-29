465bde6be54d8b5725e32d7bff62b633d9e3af7b
Ignacio Cordoba <theignaciocordoba@gmail.com>
2019-06-29 18:40:59 +0000

    Initial Commit

---
+++ src/styles/index.js
@@ -0,0 +1,23 @@
+import tinycolor from 'tinycolor2';
+
+const COLOURS = {
+  PRIMARY: '#0079af',
+  PRIMARYLIGHT: '#55a8e1',
+  PRIMARYDARK: '#004d7f',
+  SECONDARY: '#af3700',
+  SECONDARYLIGHT: '#e86731',
+  SECONDARYDARK: '#790000',
+  GREY: '#616161',
+  BLACK: '#000000',
+  WHITE: '#FFFFFF',
+  LIGHTGREY: '#F8F7F6',
+  RED: '#F44336',
+  TRANSPARENT: tinycolor({ r: 0, g: 0, b: 0, a: 0 })
+};
+
+const getColor = (colorName) => {
+  const color = COLOURS[colorName] || COLOURS['PRIMARY'];
+  return tinycolor(color).clone();
+};
+
+export default getColor;
---
+++ src/styles/material.js
@@ -0,0 +1,51 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import getColor from './';
+import tinycolor from 'tinycolor2';
+
+const theme = createMuiTheme({
+  palette: {
+    common:{
+      black: `${getColor('BLACK')}`,
+      white: `${getColor('WHITE')}`
+    },
+    primary: {
+      light: `${getColor('PRIMARYLIGHT')}`,
+      main: `${getColor('PRIMARY')}`,
+      dark: `${getColor('PRIMARYDARK')}`,
+      contrastText: `${tinycolor.mostReadable(getColor('PRIMARY'),[],{includeFallbackColors:true})}`,
+    },
+    secondary: {
+      light: `${getColor('SECONDARYLIGHT')}`,
+      main: `${getColor('SECONDARY')}`,
+      dark: `${getColor('SECONDARYDARK')}`,
+      contrastText: `${tinycolor.mostReadable(getColor('SECONDARY'),[],{includeFallbackColors:true})}`,
+    },
+    error: {
+      //light: palette.error[300],
+      main: `${getColor('RED')}`,
+      //dark: palette.error[700],
+      contrastText: `${tinycolor.mostReadable(getColor('RED'),[],{includeFallbackColors:true})}`,
+    },
+    text: {
+      primary: `${getColor('BLACK')}`,
+      secondary: `${getColor('GREY')}`,
+      disabled: `${getColor('GREY')}`
+    }
+  },
+  typography: {
+    useNextVariants: true
+  },
+  mixins: {
+    toolbar: {
+      minHeight: '56px',
+      '@media (min-width:0px) and (orientation: landscape)': {
+        minHeight: '48px',
+      },
+      '@media (min-width:600px)': {
+        minHeight: '64px'
+      }
+    }
+  }
+});
+
+export default theme;
