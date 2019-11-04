082d997fd8e16bdea05dc10a111cd2915644cc4c
Daniel Ortiz <dortizlira@gmail.com>
2019-11-04 10:23:11 -0500

    Integrate wastes and containers

--- src/app/utils/company/index.js
+++ src/app/utils/company/index.js
@@ -1,11 +1,23 @@
 export const setCompanySession = (company, wastes, containers) => {
-  sessionStorage.setItem('COMPANY', company);
-  sessionStorage.setItem('COMPANY_WASTES', wastes);
-  sessionStorage.setItem('COMPANY_CONTAINERS', containers);
+  sessionStorage.setItem('COMPANY', JSON.stringify(company));
+  sessionStorage.setItem('COMPANY_WASTES', JSON.stringify(wastes));
+  sessionStorage.setItem('COMPANY_CONTAINERS', JSON.stringify(containers));
 };
 
 export const getCompanySession = () => ({
-  company: sessionStorage.setItem('COMPANY'),
-  wastes: sessionStorage.setItem('COMPANY_WASTES'),
-  containers: sessionStorage.setItem('COMPANY_CONTAINERS')
+  company: JSON.parse(sessionStorage.getItem('COMPANY')),
+  wastes: JSON.parse(sessionStorage.getItem('COMPANY_WASTES')),
+  containers: JSON.parse(sessionStorage.getItem('COMPANY_CONTAINERS'))
 });
+
+export const deleteCompanySession = () => {
+  if (sessionStorage.getItem('COMPANY')) {
+    sessionStorage.removeItem('COMPANY');
+  }
+  if (sessionStorage.getItem('COMPANY_WASTES')) {
+    sessionStorage.removeItem('COMPANY_WASTES');
+  }
+  if (sessionStorage.getItem('COMPANY_CONTAINERS')) {
+    sessionStorage.removeItem('COMPANY_CONTAINERS');
+  }
+};
